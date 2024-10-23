eda9150ba289506c34622997e494857bc93c13a9
rittiev <rittiev@devexperts.com>
2024-10-23 21:32:35 +0100

    Form outer and title markup

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,12 @@
 import "./app.css";
+import { EvilForm } from "./form/form.tsx";
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <EvilForm />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/app/form/form.tsx
@@ -0,0 +1,9 @@
+import "./form.css";
+
+export const EvilForm = () => {
+  return (
+    <form className="evil-form">
+      <h1 className="evil-form__title">Login</h1>
+    </form>
+  );
+};
