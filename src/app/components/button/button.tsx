897d18f4c7796598828a7523e093720d4c937ab9
rittiev <rittiev@devexperts.com>
2024-10-24 12:00:18 +0100

    Removed icon

--- src/app/components/button/button.tsx
+++ src/app/components/button/button.tsx
@@ -13,15 +13,9 @@ export const Button = (props: ButtonProps) => {
 
   const classes = cn(baseClassName, isLoading && `${baseClassName}_loading`);
 
-  const iconClasses = cn(
-    `${baseClassName}__icon`,
-    isLoading && `${baseClassName}__icon_loading`,
-  );
-
   return (
     <button type="button" className={classes} {...restProps}>
       <span className={`${baseClassName}__text`}>Submit</span>
-      <div className={iconClasses}></div>
     </button>
   );
 };
